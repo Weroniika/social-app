9cf2edc7206cc827c15bfd206767d4768a52c3b7
weroniika <vburzynska@outlook.com>
2021-02-27 21:05:22 +0100

    Add Post model, get and create controllers and routes

---
+++ server/controllers/posts.js
@@ -0,0 +1,30 @@
+import Post from "../models/Post.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.status(200).json({
+      message: "get posts successfully",
+      posts: posts,
+      statusCode: 200,
+    });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+  try {
+    const postData = req.body;
+    const post = await Post.create(postData);
+    const posts = await Post.find({});
+
+    res.status(201).json({
+      message: "get posts successfully",
+      posts: posts,
+      post: post,
+    });
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
---
+++ server/routes/posts.js
@@ -0,0 +1,9 @@
+import express from "express";
+import * as postsController from "../controllers/posts.js"
+
+const router = express.Router();
+
+router.get('/', postsController.getPosts)
+router.post('/', postsController.createPost)
+
+export default router;
