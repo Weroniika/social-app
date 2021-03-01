c2c4716cab53bc8bb057cf77ae67f18e7b75943b
weroniika <vburzynska@outlook.com>
2021-03-01 09:11:14 +0100

    Add Post component and Form

--- client/src/components/Posts/index.js
+++ client/src/components/Posts/index.js
@@ -1,21 +1,22 @@
 import React from "react";
 import useStyles from "./styles";
 import { useSelector } from "react-redux";
-import {Grid} from "@material-ui/core";
+import { Grid, CircularProgress } from "@material-ui/core";
 import Post from "../Post";
 
 const Posts = () => {
   const classes = useStyles();
   const posts = useSelector((state) => state.posts);
 
-  return (
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
     <Grid className={classes.container}>
-        {posts.map((post) =>(
-            <Grid key={post._id} item xs={12} sm={6} md={6}>
-              {post.title}
-              <Post post={post}/>
-            </Grid>
-        ))}
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6} md={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
     </Grid>
   );
 };
