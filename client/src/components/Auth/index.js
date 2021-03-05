cb0053b14833c3becd4403d6c4f138c5e96c608c
weroniika <vburzynska@outlook.com>
2021-03-05 14:45:50 +0100

    Add redirect file for deplyment

--- client/src/components/Auth/index.js
+++ client/src/components/Auth/index.js
@@ -22,6 +22,9 @@ let CLIENT_ID = null;
 if (process.env.NODE_ENV === "development") {
   CLIENT_ID = require("../../secret_keys").CLIENT_ID;
 }
+
+console.log(process.env.CLIENT_ID);
+
 const initState = {
   firstName: "",
   lastName: "",
